d1b272208e4cfb43572ca10369baff4aeab1a4cf
Ömer Faruk ŞİŞMAN <osisman@innova.com.tr>
2021-07-26 16:14:42 +0300

    codes uploaded

---
+++ src/main/java/com/sisman/springsecuritydemo/UserRepository.java
@@ -0,0 +1,9 @@
+package com.sisman.springsecuritydemo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUserName(String userName);
+
+}
